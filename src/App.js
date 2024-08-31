aa815672fb597497a2024c1dab9c0005a7984c9e
lordkintaru <milinim4s@gmail.com>
2024-08-30 21:03:31 -0300

    fix:Criando o ConteinerPrincipal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import PrincipalContainer from './components/PrincipalContainer';
 import fetchDataFromAPI from './services/fetchDataFromAPI';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -11,34 +12,8 @@ function App() {
     <>
       <BrowserRouter>
         <Header fetchData={fetchDataFromAPI}/>
-        <main >
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-
-         <h3>dasdasdasds</h3>v
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-
-         <h3>dasdasdasds</h3>v
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-         <h3>dasdasdasds</h3>
-
-         <h3>dasdasdasds</h3>v
-         <h3>dasdasdasds</h3>
-
-
-        </main>
-    
-        <Footer></Footer>
+        <PrincipalContainer/>
+        <Footer/>
       </BrowserRouter>
   
     </>
---
+++ src/components/PrincipalContainer/index.js
@@ -0,0 +1,16 @@
+import style from './PrincipalContainer.module.css'
+
+function PrincipalContainer(){
+
+
+    return(
+        <main>
+            <section className={style.principal}>
+                
+            </section>
+
+        </main>
+    )
+}
+
+export default PrincipalContainer;
